12293d9c8eb8ff262b184f37778b14d566b4e161
Andrew C. Smith <andrewchristophersmith@gmail.com>
2018-03-16 12:48:14 -0700

    use ndarray and compute stuff in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
 extern crate sample;
 extern crate hound;
 extern crate vox_box;
-extern crate rulinalg;
+extern crate ndarray;
+extern crate ndarray_parallel;
 extern crate random_choice;
+extern crate serde;
+extern crate serde_pickle;
 
 use std::error::Error;
 use std::f64;
+use std::fs::File;
+use std::path::Path;
+use std::io::prelude::*;
 
 use sample::{Sample, ToSampleSlice, FromSampleSlice, window};
-use rulinalg::matrix::{Matrix, BaseMatrix, BaseMatrixMut};
-use rulinalg::norm::Euclidean;
 use random_choice::random_choice;
+use ndarray::{ArrayView, Array, Array2, AsArray, Axis, Ix2};
+use ndarray_parallel::NdarrayIntoParallelIterator;
+use ndarray_parallel::prelude::*;
 use vox_box::spectrum::MFCC;
 
-const NCOEFFS: usize = 40;
+const NCOEFFS: usize = 32;
 const BIN: usize = 2048;
-const HOP: usize = 2048;
+const HOP: usize = 512;
 const DUCK_VAL: f64 =  0.9;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -45,17 +52,34 @@ fn main() {
 
 fn go() -> Result<(), Box<Error>> {
     // mono file so I don't care whether it's interleaved
-    let mut reader = hound::WavReader::open("full_source.wav")?;
-    let samples: Vec<f64> = reader.samples::<i16>()
+    let mut reader = hound::WavReader::open("source_minus_end.wav")?;
+    let me_samples: Vec<f64> = reader.samples::<i16>()
+        .map(|s| s.unwrap().to_sample())
+        .collect();
+    let mut reader = hound::WavReader::open("target.wav")?;
+    let you_samples: Vec<f64> = reader.samples::<i16>()
         .map(|s| s.unwrap().to_sample())
         .collect();
 
-    let mfccs = analyze_mfccs(44_100.0, &samples[..]);
-    let sim = self_similarity_matrix(mfccs.clone(), mfccs);
-    // println!("sim: {:?}", sim);
+    let me_mfccs = analyze_mfccs(44_100.0, &me_samples[..]);
+    let you_mfccs = analyze_mfccs(44_100.0, &you_samples[..]);
+    let rows = me_mfccs.len() / NCOEFFS;
+    { 
+        let mfccs = ArrayView::from_shape([rows, NCOEFFS], &me_mfccs)?;
+        let serialized = serde_pickle::to_vec(&mfccs, true)?;
+        let mut pickled = File::create(Path::new("mfccs.pk"))?;
+        pickled.write_all(&serialized)?;
+    }
+
+    let sim = similarity_matrix(&me_mfccs, &you_mfccs)?;
+    { 
+        let serialized = serde_pickle::to_vec(&sim, true)?;
+        let mut pickled = File::create(Path::new("sim.pk"))?;
+        pickled.write_all(&serialized)?;
+    }
 
     let spec = hound::WavSpec {
-        channels: 2,
+        channels: 1,
         sample_rate: 44100,
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
@@ -63,70 +87,75 @@ fn go() -> Result<(), Box<Error>> {
 
     let mut writer = hound::WavWriter::create("out.wav", spec).unwrap();
 
-    // Looking for 512 frames, not 512 samples
-    let frames: Vec<usize> = (0..sim.rows()).collect();
-    let mut frame_index = 0;
-    let mut chooser = random_choice();
+    let max = sim.cols();
+    let c = 0.0;
 
-    let max = 500;
-    let c = 2.0;
+    // The list of source frames
+    let frames: Vec<usize> = (0..sim.rows()).collect();
+
+    let mut chooser = random_choice();
+    let mut frame_index = 0;
 
     let frame_indices: Vec<usize> = (0..max).map(|coeff| {
-        // let weights: Vec<f64> = sim.row(coeff).iter()
-        //     .map(|s| {
-        //         // let angular = 1.0 - (s.acos() * f64::consts::FRAC_1_PI);
-        //         // let e = ((max - coeff) as f64 / max as f64) * c - (c * 0.5);
-        //         // (s + 1.0).powf(e)
-        //         *s
-        //     }).collect();
-        // // frame_index = *chooser.random_choice_f64(&frames, &weights, 1)[0];
-        sim.row(coeff).iter().enumerate()
-            .fold((0, -1.0), |(mut midx, mut m), (sidx, s)| {
-                if *s > m {
-                    m = *s;
-                    midx = sidx;
-                }
-                (midx, m)
-            }).0
+        let weights: Vec<f64> = sim.t().row(coeff).iter()
+            .map(|s| {
+                let angular = s.acos() * f64::consts::FRAC_1_PI;
+                // let e = ((max - coeff) as f64 / max as f64) * c - (c * 0.5);
+                (1.0 - angular).powi(1000)
+            }).collect();
+        frame_index = *chooser.random_choice_f64(&frames, &weights, 1)[0];
+        frame_index
     }).collect();
 
     let win: window::Window<[f64; 1], window::Hanning> = window::Window::new(BIN);
-    for f in frame_indices.chunks(2) {
-        let chosen_left = samples[f[0]*HOP..f[0]*HOP+BIN]
-            .iter().zip(win.clone()).map(|(s, w)| s * w[0]);
-        let chosen_right = samples[f[1]*HOP..f[1]*HOP+BIN]
-            .iter().zip(win.clone()).map(|(s, w)| s * w[0]);
 
-        for (l, r) in chosen_left.zip(chosen_right) {
-            writer.write_sample(l.to_sample::<i16>())?;
-            writer.write_sample(r.to_sample::<i16>())?;
+    let out_len = frame_indices.len() * HOP + (BIN - HOP);
+    let mut out = vec![0f64; out_len];
+
+    for (idx, f) in frame_indices.iter().enumerate() {
+        let samps = &me_samples[f*HOP..f*HOP+BIN];
+        let out_start = idx * HOP;
+        for (out_idx, (s, w)) in samps.iter().zip(win.clone()).enumerate() {
+            out[out_start+out_idx] += s * w[0];
         }
     }
 
+    for s in out {
+        writer.write_sample(s.to_sample::<i16>())?;
+    }
     writer.finalize()?;
 
     Ok(())
 }
 
-fn self_similarity_matrix(input: Matrix<f64>, other: Matrix<f64>) -> Matrix<f64> {
-    assert_eq!(input.cols(), other.cols());
-    let norms1 = input.row_iter().map(|r| r.norm(Euclidean));
-    let norms2 = other.row_iter().map(|r| r.norm(Euclidean));
+fn similarity_matrix(me_slice: &[f64], you_slice: &[f64]) -> Result<Array2<f64>, Box<Error>>
+{
+    let me_shape = [me_slice.len() / NCOEFFS, NCOEFFS];
+    let you_shape = [you_slice.len() / NCOEFFS, NCOEFFS];
+    let me = ArrayView::from_shape(me_shape, me_slice)?;
+    let you = ArrayView::from_shape(you_shape, you_slice)?;
 
-    let t = other.transpose();
-    let mut d = input.clone() * t;
+    let mut me_norms = Vec::new();
+    me.axis_iter(Axis(0))
+        .into_par_iter()
+        .map(|row| row.fold(0.0, |memo, val| val.powi(2) + memo).sqrt())
+        .collect_into_vec(&mut me_norms);
 
-    for ((mut row, norm1), norm2) in d.row_iter_mut().zip(norms1).zip(norms2) {
-        let n = norm1 * norm2;
-        for x in row.raw_slice_mut().iter_mut() {
-            *x /= n;
-        }
-    }
+    let mut you_norms = Vec::new();
+    you.axis_iter(Axis(0))
+        .into_par_iter()
+        .map(|row| row.fold(0.0, |memo, val| val.powi(2) + memo).sqrt())
+        .collect_into_vec(&mut you_norms);
 
-    d
+    // This gives us a me_rows X you_rows matrix
+    let mut dot = me.dot(&you.t());
+    let norm_dot = ArrayView::from_shape([me_shape[0], 1], &me_norms)?
+        .dot(&ArrayView::from_shape([1, you_shape[0]], &you_norms)?);
+
+    Ok(dot / norm_dot)
 }
 
-fn analyze_mfccs(sample_rate: f64, samples: &[f64]) -> Matrix<f64> {
+fn analyze_mfccs(sample_rate: f64, samples: &[f64]) -> Vec<f64> {
     let mfcc_calc = |frame: &[f64]| -> [f64; NCOEFFS] { 
         let mut mfccs = [0f64; NCOEFFS];
         let m = frame.mfcc(NCOEFFS, (100., 8000.), sample_rate as f64);
@@ -153,6 +182,5 @@ fn analyze_mfccs(sample_rate: f64, samples: &[f64]) -> Matrix<f64> {
             acc.extend_from_slice(&v[..]);
             acc
         });
-
-    Matrix::new(v.len() / NCOEFFS, NCOEFFS, v)
+    v
 }
